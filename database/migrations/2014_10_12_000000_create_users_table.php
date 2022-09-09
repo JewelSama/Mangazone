5bc02c58664388a08c32b979ca01a25e2e9f775d
Jewel Osafile <osafilejewel@gmail.com>
2022-09-09 17:48:28 +0100

    users

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,7 +19,7 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->boolean('isAdmin')->default(false);
+            $table->string('role_type')->default('user');
             $table->rememberToken();
             $table->timestamps();
         });
