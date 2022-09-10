9db823c8c91e1070b8c3b1886799ba37ff329b15
Jewel Osafile <osafilejewel@gmail.com>
2022-09-10 17:29:00 +0100

    dashboard

--- resources/views/admin/dashboardLayout.blade.php
+++ resources/views/admin/dashboardLayout.blade.php
@@ -21,7 +21,7 @@
   <link rel="apple-touch-icon" sizes="76x76" href="../assets/img/apple-icon.png">
   <link rel="icon" type="image/png" href="images/manLogo.png">
   <title>
-    Mangazone Dashboard | Admin 
+    Mangazone | Admin 
   </title>
   <!--     Fonts and icons     -->
   <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700,900|Roboto+Slab:400,700" />
