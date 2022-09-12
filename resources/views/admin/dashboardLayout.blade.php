8fd33de2092efc86416c4338925a8f7cce1736b4
Jewel Osafile <osafilejewel@gmail.com>
2022-09-12 21:23:22 +0100

    layout

--- resources/views/admin/dashboardLayout.blade.php
+++ resources/views/admin/dashboardLayout.blade.php
@@ -91,22 +91,8 @@
             <span class="nav-link-text ms-1">Profile</span>
           </a>
         </li>
-        <li class="nav-item">
-          <a class="nav-link text-white " href="../pages/sign-in.html">
-            <div class="text-white text-center me-2 d-flex align-items-center justify-content-center">
-              <i class="material-icons opacity-10">login</i>
-            </div>
-            <span class="nav-link-text ms-1">Sign In</span>
-          </a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link text-white " href="../pages/sign-up.html">
-            <div class="text-white text-center me-2 d-flex align-items-center justify-content-center">
-              <i class="material-icons opacity-10">assignment</i>
-            </div>
-            <span class="nav-link-text ms-1">Sign Up</span>
-          </a>
-        </li>
+        
+        
       </ul>
     </div>
     <div class="sidenav-footer position-absolute w-100 bottom-6">
