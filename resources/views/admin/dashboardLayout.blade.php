339b9815ff6afee880a9a77a25575635ad88dc2b
Jewel Osafile <osafilejewel@gmail.com>
2022-09-12 13:35:19 +0100

    .active

--- resources/views/admin/dashboardLayout.blade.php
+++ resources/views/admin/dashboardLayout.blade.php
@@ -65,7 +65,7 @@
           </a>
         </li>
         <li class="nav-item">
-          <a class="nav-link text-white {{request()->is('manga') ? 'active bg-gradient-danger' : ''}}" href="{{route('manga')}}">
+          <a class="nav-link text-white {{request()->is('manga-table') ? 'active bg-gradient-danger' : ''}}" href="{{route('manga')}}">
             <div class="text-white text-center me-2 d-flex align-items-center justify-content-center">
               <i class="material-icons opacity-10">auto_stories</i>
             </div>
