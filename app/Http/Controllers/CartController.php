d47b4190f7005fcf0c61854df227754508b5b657
Jewel Osafile <osafilejewel@gmail.com>
2022-09-14 13:35:56 +0100

    add to cart and view cart items

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -2,29 +2,49 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Manga;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Session;
 
 class CartController extends Controller
 {
-    public function show(){
-        return view('cart');
-    }
 
     public function addCart($id){
+        // Session::remove('cart');
+        // return 1;
+
+
         if(Session::has('cart')){
             $cart = Session::get('cart');
-            array_push($cart, $id);
-            // dd($cart);
-            return back()->with('status', 'Oii!! Manga already in cart');
+            
+            $cart[] = $id;
+            $new_cart = $cart;
+            // return $new_cart;
+            
+            Session::put('cart', $new_cart);
+
+            return redirect('/cart')->with('status', 'Sugoi!! Manga Added to cart');
         } else {
             $cart = [$id];
             Session::put('cart', $cart);
             return redirect('/cart')->with('status', 'Sugoi!! Manga Added to cart');
-            dd($cart);
         }
+
+        
 
     }
 
+    public function show(){
+        $cart = Session::get('cart');
+        $total = 0;
+        $manga = [];
+        foreach($cart as $item){
+            $x = Manga::find($item);
+            $manga[] = $x;
+            $total +=$x->price;
+        }
+        // return $manga;
+        return view('cart', compact('manga', 'total'));
+    }
     
 }
