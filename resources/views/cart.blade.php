ea0ec18a2527cd0a5c47601b7ae2f7351f78a2b4
Jewel Osafile <osafilejewel@gmail.com>
2022-09-14 13:36:28 +0100

    show cart

--- resources/views/cart.blade.php
+++ resources/views/cart.blade.php
@@ -1,3 +1,4 @@
+
 @extends('layout')
 @section('content')
 <div class="container">
@@ -6,55 +7,58 @@
             <table class="table table-hover">
                 <thead>
                     <tr>
-                        <th>Product</th>
-                        <th>Quantity</th>
+                        <th>Product @php echo count($manga); @endphp </th>
+                        <!-- <th>Quantity</th> -->
                         <th class="text-center">Price</th>
                         <th class="text-center">Total</th>
                         <th> </th>
                     </tr>
                 </thead>
                 <tbody>
+                        @if (count($manga))
+                       @foreach ($manga as $item)
                     <tr>
                         <td class="col-sm-8 col-md-6">
                         <div class="media">
-                            <a class="thumbnail pull-left" href="#"> <img class="media-object" src="http://icons.iconarchive.com/icons/custom-icon-design/flatastic-2/72/product-icon.png" style="width: 72px; height: 72px;"> </a>
+                            <a class="thumbnail pull-left" href="/manga/{{$item->id}}"> <img class="media-object" src="{{asset('storage/' .$item->manga_cover)}}" style="width: 72px; height: 72px;"> </a>
                             <div class="media-body">
-                                <h4 class="media-heading"><a href="#">Product name</a></h4>
-                                <h5 class="media-heading"> by <a href="#">Brand name</a></h5>
+                                <h4 class="media-heading"><a href="#">{{$item->name}}</a></h4>
+                                <h5 class="media-heading"> by <a href="#">{{$item->category}}</a></h5>
                                 <span>Status: </span><span class="text-success"><strong>In Stock</strong></span>
                             </div>
                         </div></td>
-                        <td class="col-sm-1 col-md-1" style="text-align: center">
+                        <!-- <td class="col-sm-1 col-md-1" style="text-align: center">
                         <input type="email" class="form-control" id="exampleInputEmail1" value="6">
-                        </td>
-                        <td class="col-sm-1 col-md-1 text-center"><strong>$4.87</strong></td>
-                        <td class="col-sm-1 col-md-1 text-center"><strong>$14.61</strong></td>
+                        </td> -->
+                        <td class="col-sm-1 col-md-1 text-center"><strong>₦{{number_format($item->price)}}</strong></td>
+                        <td class="col-sm-1 col-md-1 text-center"><strong>₦{{number_format($item->price)}}</strong></td>
                         <td class="col-sm-1 col-md-1">
                         <button type="button" class="btn btn-danger">
                             <span class="glyphicon glyphicon-remove"></span> Remove
                         </button></td>
                     </tr>
-                    
+                    @endforeach 
+                    @endif
                     <tr>
                         <td>   </td>
                         <td>   </td>
                         <td>   </td>
                         <td><h5>Subtotal</h5></td>
-                        <td class="text-right"><h5><strong>$24.59</strong></h5></td>
+                        <td class="text-right"><h5><strong>₦{{number_format($total)}}</strong></h5></td>
                     </tr>
                     <tr>
                         <td>   </td>
                         <td>   </td>
                         <td>   </td>
-                        <td><h5>Estimated shipping</h5></td>
-                        <td class="text-right"><h5><strong>$6.94</strong></h5></td>
+                        <!-- <td><h5>Estimated shipping</h5></td> -->
+                        <!-- <td class="text-right"><h5><strong>n</strong></h5></td> -->
                     </tr>
                     <tr>
                         <td>   </td>
                         <td>   </td>
                         <td>   </td>
                         <td><h3>Total</h3></td>
-                        <td class="text-right"><h3><strong>$31.53</strong></h3></td>
+                        <td class="text-right"><h3><strong>₦{{number_format($total)}}</strong></h3></td>
                     </tr>
                     <tr>
                         <td>   </td>
