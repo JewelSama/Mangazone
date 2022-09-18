e1a533bb050f53295fb5f52feeebbdcc4a5b0062
Jewel Osafile <osafilejewel@gmail.com>
2022-09-18 11:47:53 +0100

    fillable

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -8,6 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class Order extends Model
 {
     use HasFactory;
+    protected $fillable=[
+        'reference',
+        'trx_ref',
+        
+    ];
 
 public function users(){
     $this->belongsTo(User::class);
